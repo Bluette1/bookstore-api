7b6f1a71e07433543e53be5769ad2e03e6c3e9a2
Bluette1 <marylene.sawyer@gmail.com>
2020-12-29 14:44:25 +0200

    Add devise gem, books route

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,31 @@
+class BookController < ApplicationController
+  # before_action :set_todo
+  before_action :set_book, only: [:destroy]
+
+  # GET /books
+  def index
+    render :json => User.all
+  end
+
+  # POST /book
+  def create
+    @book = Todo.create!(book_params)
+    render :json => @book
+  end
+
+  # DELETE /book:id
+  def destroy
+    @book.destroy
+    head :no_content
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :category, :author)
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
+  # resources :users, only: %i[new create show]
+  resources :books, only: %i[create index destroy] 
 end
