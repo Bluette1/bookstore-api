73eaafcce96e424a18bcae0c872a0839f338de09
Bluette1 <marylene.sawyer@gmail.com>
2021-01-18 06:44:22 +0200

    Add the devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'books#index'
   # resources :users, only: %i[new create show]
   resources :books, only: %i[create index destroy update] 
